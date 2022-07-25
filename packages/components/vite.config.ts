95c8bbf94c355470bfc7fe73b624fd62a169cbb5
bbbottle <z@zjh.im>
2022-07-25 13:25:22 +0800

    chore: init components package

---
+++ packages/components/vite.config.ts
@@ -0,0 +1,22 @@
+import { defineConfig } from 'vite'
+import { resolve } from 'path'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    lib: {
+      name: '@bbki.ng/components',
+      fileName: 'components',
+      entry: resolve(__dirname, 'lib/index.ts')
+    },
+    rollupOptions: {
+      external: [
+        'react',
+        'classnames',
+        'react-route-dom'
+      ]
+    }
+  }
+})
