5f8f1b5add89257f348ee85062064d483c15377d
bbbottle <z@zjh.im>
2022-07-28 13:53:21 +0800

    chore: add missing dependency

--- packages/site/src/components/img_list/index.tsx
+++ packages/site/src/components/img_list/index.tsx
@@ -1,5 +1,5 @@
 import React, { FunctionComponent, ReactElement, ReactNode } from "react";
-import cls from "classnames";
+import classnames from "classnames";
 import { Photo } from "@/types/photo";
 import { Article, Img } from "@bbki.ng/components";
 
@@ -14,13 +14,13 @@ const BaseImgList: FunctionComponent<imgListProps> = (props: imgListProps) => {
   const { imgList, className, beforeListRenderer } = props;
 
   return (
-    <div className={cls("max-h-full no-scrollbar overflow-auto", className)}>
+    <div className={classnames("max-h-full no-scrollbar overflow-auto", className)}>
       {beforeListRenderer && <>{beforeListRenderer()}</>}
       {imgList.map((img, index) => {
         const isLast = index === imgList.length - 1;
         return (
           <div key={img.src}>
-            <Img {...img} className={cls({ "mb-256": !isLast })} />
+            <Img {...img} className={classnames({ "mb-256": !isLast })} />
           </div>
         );
       })}
