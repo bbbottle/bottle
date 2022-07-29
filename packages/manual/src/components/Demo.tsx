24304184628aabe21136256b24cb3408707b80b1
bbbottle <z@zjh.im>
2022-07-30 02:06:29 +0800

    fix: demo ssr error

--- packages/manual/src/components/Demo.tsx
+++ packages/manual/src/components/Demo.tsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import BrowserOnly from '@docusaurus/BrowserOnly';
+
 
 export const Demo = ({ children }) => {
   return (
-    <div className="p-32 grid items-start h-full w-full font-mono" style={{ border: 'dotted 1px #333 '}}>
-      {children}
-    </div>
+    <BrowserOnly>
+      {() => (
+        <div className="p-32 grid items-start h-full w-full font-mono" style={{ border: 'dotted 1px #333 '}}>
+          {children}
+        </div>
+      )}
+    </BrowserOnly>
   )
 }
