a8a2c9773d4e063535f503c78ec010f58b39a00e
bbbottle <z@zjh.im>
2024-03-02 13:42:39 +0800

    fix: remove unused var

--- packages/site/src/pages/cover/index.tsx
+++ packages/site/src/pages/cover/index.tsx
@@ -3,10 +3,6 @@ import { CenterLinkList } from "@/components";
 import { Footer } from "@/components/Footer";
 
 export const Cover = (props: { className: string }) => {
-  // @ts-ignore
-  const appVer = GLOBAL_BBKING_VERSION;
-  const tagUrl = `https://github.com/bbbottle/bottle/releases/tag/@bbki.ng/site@${appVer}`;
-
   return (
     <div className="w-fit m-auto">
       <CenterLinkList
