f28c13c83e4d855fbc3e9e48cec7c56d9bf66963
bbbottle <z@zjh.im>
2024-03-09 02:24:51 +0800

    feat: add context menu

--- packages/site/src/pages/cover/index.tsx
+++ packages/site/src/pages/cover/index.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { CenterLinkList } from "@/components";
+import { ContextMenuDemo } from "@/demo/DemoMenu";
 
 export const Cover = (props: { className: string }) => {
   return (
--- packages/stylebase/lib/index.cjs
+++ packages/stylebase/lib/index.cjs
@@ -16,12 +16,22 @@ module.exports = {
     extend: {
       animation: {
         "ping-fast": "blink .1s linear infinite",
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
       },
       keyframes: {
         blink: {
           "0%": { opacity: 1 },
           "75%": { opacity: 0 },
           "100%": { opacity: 1 },
+        },
+        "accordion-down": {
+          from: { height: "0" },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: "0" },
         },
       },
       fontSize: {
@@ -89,6 +99,39 @@ module.exports = {
           500: "#EF4444",
           600: "#DC2626",
         },
+        border: "hsl(var(--border))",
+        input: "hsl(var(--input))",
+        ring: "hsl(var(--ring))",
+        background: "hsl(var(--background))",
+        foreground: "hsl(var(--foreground))",
+        primary: {
+          DEFAULT: "hsl(var(--primary))",
+          foreground: "hsl(var(--primary-foreground))",
+        },
+        secondary: {
+          DEFAULT: "hsl(var(--secondary))",
+          foreground: "hsl(var(--secondary-foreground))",
+        },
+        destructive: {
+          DEFAULT: "hsl(var(--destructive))",
+          foreground: "hsl(var(--destructive-foreground))",
+        },
+        muted: {
+          DEFAULT: "hsl(var(--muted))",
+          foreground: "hsl(var(--muted-foreground))",
+        },
+        accent: {
+          DEFAULT: "hsl(var(--accent))",
+          foreground: "hsl(var(--accent-foreground))",
+        },
+        popover: {
+          DEFAULT: "hsl(var(--popover))",
+          foreground: "hsl(var(--popover-foreground))",
+        },
+        card: {
+          DEFAULT: "hsl(var(--card))",
+          foreground: "hsl(var(--card-foreground))",
+        },
       },
       fontFamily: {
         sans: [
@@ -120,6 +163,12 @@ module.exports = {
         ],
       },
 
+      borderRadius: {
+        lg: `var(--radius)`,
+        md: `calc(var(--radius) - 2px)`,
+        sm: "calc(var(--radius) - 4px)",
+      },
+
       typography: (theme) => ({
         DEFAULT: {
           css: {
@@ -146,5 +195,6 @@ module.exports = {
     require("./plugin"),
     require("@tailwindcss/typography"),
     require("@tailwindcss/aspect-ratio"),
+    require("tailwindcss-animate"),
   ],
 };
