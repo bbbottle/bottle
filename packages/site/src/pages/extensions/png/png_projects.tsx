c5ebfc6f3ba752d9519b19a41cc860cc93117492
bbbottle <z@zjh.im>
2024-03-10 00:25:22 +0800

    fix: add menu item

--- packages/site/src/pages/extensions/png/png_projects.tsx
+++ packages/site/src/pages/extensions/png/png_projects.tsx
@@ -3,8 +3,7 @@ import { MySuspense } from "@/components";
 import { useParams } from "react-router-dom";
 import { useProjects } from "@/hooks/use_projects";
 import { imageFormatter } from "@/utils";
-import { Gallery, Nav, Link } from "@bbki.ng/components";
-import { usePaths } from "@/hooks";
+import { Gallery } from "@bbki.ng/components";
 import { ImageUploader } from "@/components/ImageUploader";
 import classnames from "classnames";
 import { ImageRenderer } from "@bbki.ng/components/lib";
@@ -21,8 +20,17 @@ const ProjectDetail = () => {
   }, []);
 
   const renderImage: ImageRenderer = (img, index, col) => {
+    const imgInfo = projects.images[index];
+
     return (
-      <ImgCtxMenu>
+      <ImgCtxMenu
+        date={imgInfo.created_at}
+        name={imgInfo.title}
+        width={imgInfo.width}
+        height={imgInfo.height}
+        id={imgInfo.id}
+        onRemoved={refresh}
+      >
         <div
           className={classnames("mb-128 select-none", {
             "md:mr-64": col === 0,
