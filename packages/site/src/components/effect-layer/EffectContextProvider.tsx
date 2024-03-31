91938eb41ff21db95e6eda528971e471d33fbf91
bbbottle <z@zjh.im>
2024-03-31 19:37:34 +0800

    fix: effect layer

---
+++ packages/site/src/components/effect-layer/EffectContextProvider.tsx
@@ -0,0 +1,15 @@
+import React, { createContext, ReactNode } from "react";
+import { EffectLayer } from "@/components/effect-layer/EffectLayer";
+
+const EffectContext = createContext<any>({});
+
+export const EffectContextProvider = (props: { children: ReactNode }) => {
+  return (
+    <EffectContext.Provider value={{}}>
+      <>
+        <EffectLayer />
+        {props.children}
+      </>
+    </EffectContext.Provider>
+  );
+};
