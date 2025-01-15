0312751942411fe5296849cef5679e762cfb8a44
bbking <b@bbki.ng>
2025-01-15 02:09:11 +0000

    fix: show entry after plugin init

--- packages/site/src/blog/components/app_ctx_menu/index.tsx
+++ packages/site/src/blog/components/app_ctx_menu/index.tsx
@@ -1,4 +1,4 @@
-import React, { ReactElement } from "react";
+import React, { ReactElement, useEffect } from "react";
 import {
   ContextMenu,
   ContextMenuContent,
@@ -11,8 +11,24 @@ import { LoginMenuItem } from "@/components/app_ctx_menu/LoginMenuItem";
 import { VersionMenuItem } from "@/components/app_ctx_menu/VersionMenuItem";
 import { ViewSourceMenuItem } from "@/components/app_ctx_menu/ViewSourceMenuItem";
 import { PluginsMenuItem } from "@/components/plugin/PluginsMenuItem";
+import { PluginManager } from "@/plugin/PluginManager";
+import { PluginEvent } from "@/plugin/PluginEvent";
 
 export const AppCtxMenu = (props: { children: ReactElement }) => {
+  const [showPluginEntry, setShowPluginEntry] = React.useState(false);
+
+  const showEntry = () => {
+    setShowPluginEntry(true);
+  }
+
+  useEffect(() => {
+    PluginManager.addEventListener(PluginEvent.INIT, showEntry);
+
+    return () => {
+      PluginManager.removeEventListener(PluginEvent.INIT, showEntry);
+    }
+  }, [])
+
   return (
     <ContextMenu>
       <ContextMenuTrigger>{props.children}</ContextMenuTrigger>
@@ -20,8 +36,12 @@ export const AppCtxMenu = (props: { children: ReactElement }) => {
         <LoginMenuItem />
         <VersionMenuItem />
         <ViewSourceMenuItem />
-        <ContextMenuSeparator />
-        <PluginsMenuItem />
+        {showPluginEntry && (
+          <>
+            <ContextMenuSeparator />
+            <PluginsMenuItem />
+          </>
+        )}
       </ContextMenuContent>
     </ContextMenu>
   );
