73bc724585222755c12cb0e5d1db92ab4f7b4f22
bbbottle <z@zjh.im>
2025-01-18 12:13:46 +0800

    fix: menu icon align

--- packages/site/src/blog/components/plugin/PluginsMenuItem.tsx
+++ packages/site/src/blog/components/plugin/PluginsMenuItem.tsx
@@ -18,7 +18,7 @@ export const PluginsMenuItem = () => {
   }, []);
   return (
     <ContextMenuSub>
-      <ContextMenuSubTrigger inset>
+      <ContextMenuSubTrigger>
         <svg
           xmlns="http://www.w3.org/2000/svg"
           width="16"
@@ -28,14 +28,20 @@ export const PluginsMenuItem = () => {
           stroke="currentColor"
           strokeWidth="2"
           strokeLinecap="round"
-          strokeLinejoin="round"
-          className="lucide lucide-plug rotate-90 absolute left-2.5"
-      >
-        <path d="M12 22v-5"></path>
-        <path d="M9 8V2"></path>
-        <path d="M15 8V2"></path>
-        <path d="M18 8v5a4 4 0 0 1-4 4h-4a4 4 0 0 1-4-4V8Z"></path>
-      </svg>
+          stroke-linejoin="round"
+          className="feather feather-cpu mr-8"
+        >
+          <rect x="4" y="4" width="16" height="16" rx="2" ry="2"></rect>
+          <rect x="9" y="9" width="6" height="6"></rect>
+          <line x1="9" y1="1" x2="9" y2="4"></line>
+          <line x1="15" y1="1" x2="15" y2="4"></line>
+          <line x1="9" y1="20" x2="9" y2="23"></line>
+          <line x1="15" y1="20" x2="15" y2="23"></line>
+          <line x1="20" y1="9" x2="23" y2="9"></line>
+          <line x1="20" y1="14" x2="23" y2="14"></line>
+          <line x1="1" y1="9" x2="4" y2="9"></line>
+          <line x1="1" y1="14" x2="4" y2="14"></line>
+        </svg>
         Plugins
       </ContextMenuSubTrigger>
       <ContextMenuSubContent className="w-48">
