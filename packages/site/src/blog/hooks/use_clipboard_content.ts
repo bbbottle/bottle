cec7a7067e3abc2606e2ae9b8b1f3ee0ffa08ff8
bbbottle <b@bbki.ng>
2025-07-19 15:59:45 +0800

    feat: paste to post

---
+++ packages/site/src/blog/hooks/use_clipboard_content.ts
@@ -0,0 +1,21 @@
+import React from "react";
+
+export const useClipboardContent = () => {
+    const [clipboardContent, setClipboardContent] = React.useState<string | null>(null);
+
+    React.useEffect(() => {
+        const handlePaste = (event: ClipboardEvent) => {
+            if (event.clipboardData) {
+                setClipboardContent(event.clipboardData.getData("text/plain"));
+            }
+        };
+
+        document.addEventListener("paste", handlePaste);
+
+        return () => {
+            document.removeEventListener("paste", handlePaste);
+        };
+    }, []);
+
+    return clipboardContent;
+}
