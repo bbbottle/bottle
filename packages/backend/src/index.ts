65b65e385214663f1bdf0a355818dac2bbb25df6
bbbottle <b@bbki.ng>
2025-12-01 18:51:12 +0800

    fix: db sql

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -22,19 +22,21 @@ app.post(
 
     try {
       let { results } = await c.env.DB.prepare(
-        `INSERT INTO comment (article_id, author, content) VALUES (${articleId}, ${author}, ${content})`,
-      ).run();
+        "INSERT INTO comment (article_id, author, content, created_at) VALUES (?, ?, ?, ?);",
+      )
+        .bind(articleId, author, content, new Date().toISOString())
+        .run();
 
       return c.json({
         status: "success",
         message: "Comment added successfully",
         results,
       });
-    } catch (error) {
+    } catch (error: any) {
       return c.json({
         status: "error",
         message: "Failed to add comment",
-        error,
+        error: error.message,
       });
     }
   },
