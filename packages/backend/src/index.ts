b305c6c4431069300e40a9285b330ee3862a34b6
bbbottle <b@bbki.ng>
2025-12-01 18:33:56 +0800

    chore: test cors

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -1,5 +1,6 @@
 import { Hono } from "hono";
 import type { D1Database } from "@cloudflare/workers-types";
+import { cors } from "hono/cors";
 
 type Bindings = {
   DB: D1Database;
@@ -11,22 +12,32 @@ app.get("/", (c) => {
   return c.text("Hello Hono!");
 });
 
-app.post("/comment/add", async (c) => {
-  const { articleId, author, content } = await c.req.json();
+app.post(
+  "/comment/add",
+  cors({
+    origin: ["https://bbki.ng"],
+  }),
+  async (c) => {
+    const { articleId, author, content } = await c.req.json();
 
-  try {
-    let { results } = await c.env.DB.prepare(
-      `INSERT INTO comment (article_id, author, content) VALUES (${articleId}, ${author}, ${content})`,
-    ).run();
+    try {
+      let { results } = await c.env.DB.prepare(
+        `INSERT INTO comment (article_id, author, content) VALUES (${articleId}, ${author}, ${content})`,
+      ).run();
 
-    return c.json({
-      status: "success",
-      message: "Comment added successfully",
-      results,
-    });
-  } catch (error) {
-    return c.json({ status: "error", message: "Failed to add comment", error });
-  }
-});
+      return c.json({
+        status: "success",
+        message: "Comment added successfully",
+        results,
+      });
+    } catch (error) {
+      return c.json({
+        status: "error",
+        message: "Failed to add comment",
+        error,
+      });
+    }
+  },
+);
 
 export default app;
