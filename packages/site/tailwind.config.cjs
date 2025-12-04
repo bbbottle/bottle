bea170ad89cf3540df7ccd72ad9efca74aefa3a3
bbbottle <b@bbki.ng>
2025-12-04 21:27:48 +0800

    chore: fix config

--- packages/site/tailwind.config.cjs
+++ packages/site/tailwind.config.cjs
@@ -1,10 +1,4 @@
 module.exports = {
   mode: "jit",
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx, mdx}",
-  ],
-  presets: [
-    require('@bbki.ng/stylebase')
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx, mdx}"],
 };
