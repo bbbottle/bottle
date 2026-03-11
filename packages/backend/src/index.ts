d1689744a1effe18e1067e8014df71976ab64bb9
bbbottle <b@bbki.ng>
2026-03-11 22:41:41 +0800

    chore: remove unused routes

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -1,15 +1,13 @@
-import app from "./config/app.config";
+import app from './config/app.config';
 
-import { commentRouter } from "./routes/comment.routes";
-import { authRoutes } from "./routes/authn.routes";
-import { streamingRouter } from "./routes/streaming.routes";
+import { commentRouter } from './routes/comment.routes';
+import { streamingRouter } from './routes/streaming.routes';
 
-app.route("comment", commentRouter);
-app.route("auth", authRoutes);
-app.route("streaming", streamingRouter);
+app.route('comment', commentRouter);
+app.route('streaming', streamingRouter);
 
-app.get("/", (c) => {
-  return c.text("Hello Hono!");
+app.get('/', c => {
+  return c.text('Hello Hono!');
 });
 
 export default app;
