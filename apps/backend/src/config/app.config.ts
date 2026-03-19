6413a365ca1daaec6d2000850431e544cf348f70
bbbottle <b@bbki.ng>
2026-03-19 19:15:47 +0800

    fix: cors

--- apps/backend/src/config/app.config.ts
+++ apps/backend/src/config/app.config.ts
@@ -12,7 +12,10 @@ const app = new Hono<{ Bindings: Bindings }>();
 app.use(
   '*',
   cors({
-    origin: ['https://bbki.ng', '*'],
+    origin: origin => {
+      // Allow bbki.ng and any localhost port
+      return /^https:\/\/bbki\.ng$|^http:\/\/localhost(:\d+)?$/.test(origin) ? origin : null;
+    },
   })
 );
 
